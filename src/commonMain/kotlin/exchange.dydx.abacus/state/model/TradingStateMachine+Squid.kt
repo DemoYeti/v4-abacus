13f0b0eb63a6c538b6a0c83edb6fa21aa203e58b
jeremy lee <jeremy@dydx.exchange>
2024-06-06 00:47:04 +0300

    suppres error

--- src/commonMain/kotlin/exchange.dydx.abacus/state/model/TradingStateMachine+Squid.kt
+++ src/commonMain/kotlin/exchange.dydx.abacus/state/model/TradingStateMachine+Squid.kt
@@ -36,6 +36,7 @@ internal fun TradingStateMachine.squidV2SdkInfo(payload: String): StateChanges? 
     }
 }
 
+@Suppress("ForbiddenComment")
 // TODO: rename this to routerRoute/routerMsg/routerTxPayload
 internal fun TradingStateMachine.squidRoute(
     payload: String,
